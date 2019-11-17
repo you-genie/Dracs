694416aea765f3fa36da77d3607116fd84ae28c1
Genne Chung <krista2811@naver.com>
2019-11-17 13:02:32 +0900

    [#1] Basic setup completed for router.

--- app/src/main.js
+++ app/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import store from './store'
+import router from './router/router'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
   store,
   render: h => h(App)
 }).$mount('#app')
