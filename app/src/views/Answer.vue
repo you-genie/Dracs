76ca20d91071ddc3395a20c0815804c90ac58f15
Genne Chung <krista2811@naver.com>
2019-11-17 13:27:41 +0900

    [#2] Made Answer
    * Answer(view) made
    * router setup for Answer/:answer_num

---
+++ app/src/views/Answer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="Answer"> 
+<!--     <banner />
+    <answer-card />
+    <answer-board /> -->
+    </div>
+</template>
+
+<script>
+// this is page for answer.
+// import banner, answer-card, answer-board component. Fill in banner plz.
+    export default {
+        name: 'Answer'
+    }
+</script>
