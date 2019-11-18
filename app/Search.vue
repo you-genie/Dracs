5209061b4f70d3c136cc0fb8a1e2598397e8a787
Genne Chung <krista2811@naver.com>
2019-11-18 18:06:45 +0900

    [#4] Added Search template vue

---
+++ app/Search.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="Search"> 
+<!--     <banner />
+    <search-card />
+    <answer-board /> -->
+    </div>
+</template>
+
+<script>
+// this is page for search.
+// import banner, search-card(containing search query and selection cards, question-board(with question-cards(component)) component. Fill in banner plz.
+    export default {
+        name: 'Search',
+        components: {
+        }
+    }
+</script>
