5a6ee3919790fb99b1d7cfaa9f09fa22e6bf2d5c
Jaeryoung Ka <jaeryoung.ka@kaist.ac.kr>
2019-11-21 01:36:19 +0900

    importing DB from firestore

--- app/src/main.js
+++ app/src/main.js
@@ -3,8 +3,19 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import store from './store'
 import router from './router/router'
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 Vue.config.productionTip = false
+
+Vue.use(VueFire)
+firebase.initializeApp({
+  projectId: 'cs473-dragcs',
+  databaseURL: 'https://cs473-dragcs.firebaseio.com'
+})
+
+export const db = firebase.firestore()
 
 new Vue({
   vuetify,
