6a77a2bf9fbca229d4504e6c70f68b42ce2845b6
jaeryoungka <jaeryoung.ka@gmail.com>
2019-11-23 00:21:22 +0900

    implemented search query API

--- app/src/main.js
+++ app/src/main.js
@@ -3,23 +3,26 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import store from './store'
 import router from './router/router'
-import VueFire from 'vuefire'
+import { firestorePlugin } from 'vuefire'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 Vue.config.productionTip = false
 
-Vue.use(VueFire)
+Vue.use(firestorePlugin)
 firebase.initializeApp({
-  projectId: 'cs473-dragcs',
-  databaseURL: 'https://cs473-dragcs.firebaseio.com'
+    projectId: 'cs473-dragcs',
+    databaseURL: 'https://cs473-dragcs.firebaseio.com'
 })
 
 export const db = firebase.firestore()
 
+/* eslint-disable no-console */
+store.dispatch('getSearchData', [1, -1, -1, -1, "HCI"])
+    /* eslint-enable no-console */
 new Vue({
-  vuetify,
-  router,
-  store,
-  render: h => h(App)
+    vuetify,
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,15 +1,71 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { db } from '../main'
 
 Vue.use(Vuex)
-
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        fitArticleList: [],
+    },
+
+    mutations: {
+        fillFitList(state, payload) {
+            let temp = []
+            for (var i = 0, len = payload.length; i < len; i++) {
+                temp.push(payload[i][0])
+            }
+            state.fitArticleList = temp
+        }
+    },
+    actions: {
+
+        getSearchData(context, payload) {
+            let currentSemester = payload[0]
+            let major = payload[1]
+            let doubleMajor = payload[2]
+            let minor = payload[3]
+            let interestedArea = payload[4]
+            let userIDList = []
+            let fitList = []
+
+            db.collection("questions")
+                .orderBy("questionID", "asc").get()
+                .then(snapshot => {
+                    snapshot.forEach(doc => {
+                        userIDList.push(doc.data().userID)
+                    });
+                })
+                .then(function() {
+                    userIDList.forEach(userID => {
+                        db.collection("users")
+                            .where("userID", "==", userID).get()
+                            .then(snapshot => {
+                                snapshot.forEach(doc => {
+                                    let user = doc.data()
+                                    fitList.push([user.userID, Math.pow(Math.abs(currentSemester - user.currentSemester), 1.4) * -1 +
+                                        (major == user.major ? 4 : 0) + (minor == user.minor && user.minor != -1 ? 3 : 0) +
+                                        (doubleMajor == user.doubleMajor && user.doubleMajor != -1 ? 4 : 0) +
+                                        (user.reputationPts > 30 ? 3 : user.reputationPts / 10) +
+                                        ((interestedArea == user.interestedArea) == true ? 6 : 0)
+                                    ])
+                                })
+                            }).then(function() {
+                                fitList = fitList.sort(function(a, b) {
+                                    return -a[1] + b[1];
+                                }); // sort 
+                                context.commit('fillFitList', fitList)
+                            })
+                    })
+                });
+        },
+
+    },
+    modules: {},
+
+    getters: {
+        fitArticles: state => {
+            return state.fitArticleList
+        }
+    }
+
 })
